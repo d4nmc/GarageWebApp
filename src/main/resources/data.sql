3f7a32f4db21a0b4ba66e74e772345bd855625f5
d4nmc <danielmcgee.work@gmail.com>
2021-08-20 16:19:40 +0100

    mc: BACK END WORKING ON POSTMAN

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `Vehicle` (`bhp`, `brand`, `colour`, `engine_style`, `fuel_type`, `model`, `modified`, `reg_plate`)
-VALUES
-('234', 'Honda', 'Alablaster Silver', 'I4 Vtec', 'Petrol 97 RON','Civic Type R', true, 'SF57 SYY'),
-('340', 'Subaru', 'Washing Machine White', 'V8 LS3', 'Petrol 97 RON','Forester V8', true, 'SM02 FAY'),
-('340', 'Volvo', 'Obsidian Black', 'I4 + Hybrid', 'Petrol 97 RON','V60', false, 'N11 NKZ');
