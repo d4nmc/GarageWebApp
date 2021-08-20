5b92eee6655553b273748cbbbf1b31f9a2e07417
d4nmc <danielmcgee.work@gmail.com>
2021-08-20 12:32:51 +0100

    mc: BACKEND not working fully, moved to front end

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@ INSERT INTO `Vehicle` (`bhp`, `brand`, `colour`, `engine_style`, `fuel_type`, `m
 VALUES
 ('234', 'Honda', 'Alablaster Silver', 'I4 Vtec', 'Petrol 97 RON','Civic Type R', true, 'SF57 SYY'),
 ('340', 'Subaru', 'Washing Machine White', 'V8 LS3', 'Petrol 97 RON','Forester V8', true, 'SM02 FAY'),
-('340', 'Volvo', 'Obsididian Black', 'I4 + Hybrid', 'Petrol 97 RON','V60', false, 'N11 NKZ');
+('340', 'Volvo', 'Obsidian Black', 'I4 + Hybrid', 'Petrol 97 RON','V60', false, 'N11 NKZ');
