2adad12f013d895477532f011f2eccca74a390c7
d4nmc <danielmcgee.work@gmail.com>
2021-08-20 09:37:23 +0100

    mc: copying in service from previous build

--- src/main/java/com/natwest/GarageWebApp/service/GarageService.java
+++ src/main/java/com/natwest/GarageWebApp/service/GarageService.java
@@ -1,8 +1,32 @@
 package com.natwest.GarageWebApp.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.natwest.GarageWebApp.model.Vehicle;
 
 @Service
 public class GarageService {
+
+	public Vehicle createVehicle(Vehicle vehicle) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Vehicle> readAllVehicle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Vehicle updateVehicle(Long id, Vehicle vehicle) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deleteVehicle(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
