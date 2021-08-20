1c1b97363694b07bf59df6d5f8d0e50e172772e1
d4nmc <danielmcgee.work@gmail.com>
2021-08-20 11:53:37 +0100

    mc: adding in entries for sql, issue with vehicles not being found

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
-INSERT INTO `vehicle` (	
+INSERT INTO `Vehicle` (`bhp`, `brand`, `colour`, `engine_style`, `fuel_type`, `model`, `modified`, `reg_plate`)
+VALUES
+('234', 'Honda', 'Alablaster Silver', 'I4 Vtec', 'Petrol 97 RON','Civic Type R', true, 'SF57 SYY'),
+('340', 'Subaru', 'Washing Machine White', 'V8 LS3', 'Petrol 97 RON','Forester V8', true, 'SM02 FAY'),
+('340', 'Volvo', 'Obsididian Black', 'I4 + Hybrid', 'Petrol 97 RON','V60', false, 'N11 NKZ');
