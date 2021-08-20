210cd409d99fd03092a4e73697d709ce21431915
d4nmc <danielmcgee.work@gmail.com>
2021-08-20 11:01:46 +0100

    mc: create/add vehicle not working through postman internal500 error, still persistent

--- src/main/java/com/natwest/GarageWebApp/controller/GarageController.java
+++ src/main/java/com/natwest/GarageWebApp/controller/GarageController.java
@@ -1,7 +1,6 @@
 package com.natwest.GarageWebApp.controller;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
